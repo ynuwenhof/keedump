561e7bb52b826d8f1eaa119c3e5a939bd0ae7266
Yannic Nuwenhof <ynuwenhof@pm.me>
2023-05-20 17:18:18 +0200

    Add input file path option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, env = "KEEDUMP_INPUT", value_name = "FILE")]
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
